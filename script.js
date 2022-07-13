a0153163841e966427a5e69b6c7f9d3992b63a76
HiagoP <hiagoperonip@gmail.com>
2022-07-13 18:07:00 -0300

    Estruturação completa do Projeto

---
+++ script.js
@@ -0,0 +1,13 @@
+// const email = document.getElementById('email');
+// const senha = document.getElementById('password');
+// const botaoEntrar = document.getElementById('entrar');
+// const checkAceito = document
+
+// function validacao() {
+//   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
+//     alert('Olá Tryber');
+//   } else {
+//     alert('Email ou senha inválidos.');
+//   }
+// }
+// botaoEntrar.addEventListener('click', validacao);
