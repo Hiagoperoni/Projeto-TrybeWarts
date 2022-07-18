18f0bea53fb773bc144369bb04249a15eab776ad
HiagoP <hiagoperonip@gmail.com>
2022-07-17 22:41:23 -0300

    Começo da resolução do requisito 21

--- script.js
+++ script.js
@@ -5,6 +5,17 @@ const botaoSubmit = document.getElementById('submit-btn');
 const checkInfo = document.getElementById('agreement');
 const textArea = document.getElementById('textarea');
 const contador = document.getElementById('counter');
+// const formulario = document.getElementById('evaluation-form');
+// const nomeFeito = document.getElementById('nome');
+// const emailFeito = document.getElementById('email');
+// const casaEscolhida = document.getElementById('casaEscolhida');
+// const familiaEscolhida = document.getElementById('familiaEscolhida');
+// const materiaSelecionadas = document.getElementById('materiaSelecionadas');
+// const avaliacaoFeita = document.getElementById('avaliacaoFeita');
+// const observacoes = document.getElementById('observacoes');
+// const nomeEscrito = document.getElementById('input-name').value;
+// const sobrenomeEscrito = document.getElementById('input-name').value;
+// const sobrenomeEscrito = document.getElementById('input-name').value;
 
 botaoLogin.addEventListener('click', (event) => {
   event.preventDefault();
@@ -29,3 +40,18 @@ textArea.addEventListener('input', () => {
   const resultado = contador.value - textArea.value.length;
   contador.innerText = resultado;
 });
+
+// botaoSubmit.addEventListener('click', (e) => {
+//   e.preventDefault();
+//   formulario.style.display = 'none';
+//   nomeFeito.innerHTML =
+
+// });
+
+// const nomeFeito = document.getElementById('nome');
+// const emailFeito = document.getElementById('email');
+// const casaEscolhida = document.getElementById('casaEscolhida');
+// const familiaEscolhida = document.getElementById('familiaEscolhida');
+// const materiaSelecionadas = document.getElementById('materiaSelecionadas');
+// const avaliacaoFeita = document.getElementById('avaliacaoFeita');
+// const observacoes = document.getElementById('observacoes');
